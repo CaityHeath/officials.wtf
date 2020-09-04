8fb989c6247d15a537a0531a48b8ce5d3f0f12d4
CaityHeath <caity.heath@gmail.com>
2020-09-04 03:41:00 -0700

    retrieving data from backend

--- officials.wtf/src/App.js
+++ officials.wtf/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import superagent from 'superagent'
 import './App.css';
 
 function App() {
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('submit clicked')
+    let url = 'http://localhost:3000'
+    let address = e.target.userAddress.value
+    let data = await superagent.get(`${url}/findreps`).query({ data: address });
+    console.log(data);
   }
 
   return (
     <div className="App">
      <form onSubmit={handleSubmit}>
-       <input type="text" className="address-input" placeholder="Address"/>
+       <input type="text" className="address-input" placeholder="Address" name="userAddress"/>
      </form>
     </div>
   );
